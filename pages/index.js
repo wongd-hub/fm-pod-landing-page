81c373a25498bb6ab1318e5c4c2d48bd8fbf6fae
Darren Wong <darrenwong@Darrens-MacBook-Air.local>
2022-02-13 14:20:04 +1100

    Tablet MVP completed

--- pages/index.js
+++ pages/index.js
@@ -52,6 +52,7 @@ export default function Home() {
               <Image
                   src={bgPattern}
                   alt=""
+                  layout='fixed'
               />
             </div>
           </div>
